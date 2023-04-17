3702e3dd2568265c9b0f435985e6898463ae3b0b
SemPlaatsman <semplaatsman@gmail.com>
2023-04-17 20:46:48 +0200

    Initial commit

---
+++ src/test/java/com/groep2/codegeneration/CodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groep2.codegeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f364fac66e57aa4b04fc6f98638c2cfbb88bbcd5
LuukBakkum <luuk.bakkum2000@gmail.com>
2023-04-25 17:19:42 +0200

    updated the Transaction files. Should work now. Needs more testing

--- src/main/java/nl/inholland/codegeneration/controllers/TransactionController.java
+++ src/main/java/nl/inholland/codegeneration/controllers/TransactionController.java
@@ -1,38 +1,63 @@
 package nl.inholland.codegeneration.controllers;
 
-import nl.inholland.codegeneration.models.Transaction;
-import nl.inholland.codegeneration.repositories.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import nl.inholland.codegeneration.models.Transaction;
+import nl.inholland.codegeneration.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/transactions")
 public class TransactionController {
+
     @Autowired
-    private TransactionRepository transactionRepository;
+    private TransactionService transactionService;
 
-    @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Transaction>> getAll() {
-        List<Transaction> transactions = transactionRepository.findAll();
-        return ResponseEntity.status(200).body(transactions);
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getTransactions();
     }
 
-    @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Transaction> insertTransaction(@RequestBody Transaction transaction) {
-        Transaction _transaction = transactionRepository.save(new Transaction(null, transaction.getTimestamp(), transaction.getAccountFrom(), transaction.getAccountTo(), transaction.getAmount(), transaction.getPerformingUser(), transaction.getDescription(), false));
-        return ResponseEntity.status(201).body(_transaction);
+    @GetMapping("/{id}")
+    public Transaction getTransactionById(@PathVariable int id) {
+        return transactionService.getTransactionId(id);
     }
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Transaction> deleteTransaction(@PathVariable Long id) {
-        Transaction transaction = transactionRepository.findById(id).get();
-        transaction.setIsDeleted(true);
-        transactionRepository.save(transaction);
-        return ResponseEntity.status(200).body(transaction);
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return transactionService.createTransaction(transaction);
     }
 
+    @PutMapping("/{id}")
+    public Transaction updateTransaction(@RequestBody Transaction transaction, @PathVariable int id) {
+        return transactionService.updateTransaction(transaction, id);
+    }  
+
+    @PutMapping("/{id}/delete")
+    public Transaction markTransactionAsDeleted(@PathVariable int id) {
+        return transactionService.markTransactionAsDeleted(id);
+    }
 }
+    // @Autowired
+    // private TransactionRepository transactionRepository;
+
+    // @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    // public ResponseEntity<List<Transaction>> getAll() {
+    //     List<Transaction> transactions = transactionRepository.findAll();
+    //     return ResponseEntity.status(200).body(transactions);
+    // }
+
+    // @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    // public ResponseEntity<Transaction> insertTransaction(@RequestBody Transaction transaction) {
+    //     Transaction _transaction = transactionRepository.save(new Transaction(null, transaction.getTimestamp(), transaction.getAccountFrom(), transaction.getAccountTo(), transaction.getAmount(), transaction.getPerformingUser(), transaction.getDescription(), false));
+    //     return ResponseEntity.status(201).body(_transaction);
+    // }
+
+    // @PutMapping(path = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    // public ResponseEntity<Transaction> deleteTransaction(@PathVariable Long id) {
+    //     Transaction transaction = transactionRepository.findById(id).get();
+    //     transaction.setIsDeleted(true);
+    //     transactionRepository.save(transaction);
+    //     return ResponseEntity.status(200).body(transaction);
+    // }

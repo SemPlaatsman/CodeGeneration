d2ad574a146ef008cb20b73454f926ec45ce7f89
LuukBakkum <luuk.bakkum2000@gmail.com>
2023-04-26 13:37:08 +0200

    updated the transaction classes

--- src/main/java/nl/inholland/codegeneration/controllers/TransactionController.java
+++ src/main/java/nl/inholland/codegeneration/controllers/TransactionController.java
@@ -1,5 +1,7 @@
 package nl.inholland.codegeneration.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import nl.inholland.codegeneration.models.Transaction;
 import nl.inholland.codegeneration.services.TransactionService;
@@ -14,50 +16,59 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
-    @GetMapping
-    public List<Transaction> getAllTransactions() {
-        return transactionService.getTransactions();
-    }
-
-    @GetMapping("/{id}")
-    public Transaction getTransactionById(@PathVariable int id) {
-        return transactionService.getTransactionId(id);
-    }
-
-    @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        return transactionService.createTransaction(transaction);
-    }
-
-    @PutMapping("/{id}")
-    public Transaction updateTransaction(@RequestBody Transaction transaction, @PathVariable int id) {
-        return transactionService.updateTransaction(transaction, id);
-    }  
-
-    @PutMapping("/{id}/delete")
-    public Transaction deleteTransaction(@PathVariable int id) {
-        return transactionService.deleteTransaction(id);
-    }
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getAll() {
+        try {
+            List<Transaction> transactions = transactionService.getAll();
+            return ResponseEntity.status(200).body(transactions);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }    
+    
+    @GetMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getById(@PathVariable int id) {
+        try {
+            Transaction transaction = transactionService.getById(id);
+            return ResponseEntity.status(200).body(transaction);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }    
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity add(@RequestBody Transaction transaction) {
+        try {
+            Transaction addedTransaction = transactionService.add(transaction);
+            return ResponseEntity.status(201).body(addedTransaction);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }    
+    
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity update(@RequestBody Transaction transaction, @PathVariable int id) {
+        try {
+            Transaction updatedTransaction = transactionService.update(transaction, id);
+            return ResponseEntity.status(200).body(updatedTransaction);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }    
 }
-    // @Autowired
-    // private TransactionRepository transactionRepository;
-
-    // @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<List<Transaction>> getAll() {
-    //     List<Transaction> transactions = transactionRepository.findAll();
-    //     return ResponseEntity.status(200).body(transactions);
-    // }
-
-    // @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Transaction> insertTransaction(@RequestBody Transaction transaction) {
-    //     Transaction _transaction = transactionRepository.save(new Transaction(null, transaction.getTimestamp(), transaction.getAccountFrom(), transaction.getAccountTo(), transaction.getAmount(), transaction.getPerformingUser(), transaction.getDescription(), false));
-    //     return ResponseEntity.status(201).body(_transaction);
-    // }
-
-    // @PutMapping(path = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Transaction> deleteTransaction(@PathVariable Long id) {
-    //     Transaction transaction = transactionRepository.findById(id).get();
-    //     transaction.setIsDeleted(true);
-    //     transactionRepository.save(transaction);
-    //     return ResponseEntity.status(200).body(transaction);
-    // }
+// @GetMapping
+// public List<Transaction> getAllTransactions() {
+// return transactionService.getTransactions();
+// }
+// public Transaction getById(@PathVariable int id) {
+// return transactionService.getById(id);
+// }
+// @PostMapping
+// public Transaction createTransaction(@RequestBody Transaction transaction) {
+// return transactionService.add(transaction);
+// }
+// @PutMapping("/{id}")
+// public Transaction update(@RequestBody Transaction transaction, @PathVariable
+// int id) {
+// return transactionService.update(transaction, id);
+// }

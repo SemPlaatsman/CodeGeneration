40581409a0876c6d0f7dddba9b40009a8102ef88
Floortje <florian.tjeertes@ziggo.nl>
2023-05-26 22:47:49 +0200

    update

--- src/main/java/nl/inholland/codegeneration/controllers/AccountController.java
+++ src/main/java/nl/inholland/codegeneration/controllers/AccountController.java
@@ -1,6 +1,8 @@
 package nl.inholland.codegeneration.controllers;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import nl.inholland.codegeneration.exceptions.APIException;
 import nl.inholland.codegeneration.models.Account;
 import nl.inholland.codegeneration.models.QueryParams;
+import nl.inholland.codegeneration.models.Role;
 import nl.inholland.codegeneration.models.Transaction;
 import nl.inholland.codegeneration.models.User;
 import nl.inholland.codegeneration.services.AccountService;
@@ -31,37 +34,30 @@ import nl.inholland.codegeneration.services.AccountService;
 public class AccountController {
 
     @Autowired
-    private  AccountService accountService;
-
-  
+    private AccountService accountService;
 
     // get /accounts
     @PreAuthorize("hasAuthority('CUSTOMER') OR hasAuthority('EMPLOYEE')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAll(@RequestParam(value = "filter", required = false) String filterQuery) throws Exception {
-        
-            QueryParams queryParams = new QueryParams(Transaction.class);
-            queryParams.setFilter(filterQuery);
-            List<Account> accounts = accountService.getAll(queryParams);
-            return ResponseEntity.status(200).body(accounts);
-        
-    }
+    public ResponseEntity<?> getAll(@RequestParam(value = "filter", required = false) String filterQuery)
+            throws Exception {
 
+        QueryParams queryParams = new QueryParams(Transaction.class);
+        queryParams.setFilter(filterQuery);
+        List<Account> accounts = accountService.getAll(queryParams);
+        return ResponseEntity.status(200).body(accounts);
+
+    }
 
     // get /accounts/{Iban}
     @PreAuthorize("hasAuthority('CUSTOMER') OR hasAuthority('EMPLOYEE')")
     @GetMapping(path = "/{iban}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAccountByIban(@PathVariable("iban") String iban) throws APIException {
-    
-            User user = (User)  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ResponseEntity<?> getAccountByIban(@PathVariable("iban") String Iban) throws APIException {
 
-            Account account = accountService.getAccountByIban(iban).orElseThrow();
-            if(account.getUser() !=  user){
-                throw new  APIException("no acces", HttpStatus.FORBIDDEN, null);
-            }
-            return ResponseEntity.status(200).body(account);
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Account account = CustomerIbanCheck(user, Iban);
+        return ResponseEntity.status(200).body(account);
 
-      
     }
 
     // post /accounts
@@ -69,50 +65,61 @@ public class AccountController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> insertAccount(@RequestBody Account account) {
 
-            account.setUser((User)  SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-            Account _account = accountService.insertAccount(account);
-            return ResponseEntity.status(201).body(_account);
+        account.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        Account _account = accountService.insertAccount(account);
+        return ResponseEntity.status(201).body(_account);
 
-       
     }
-    
+
     // put /accounts/{Ibans}
     @PreAuthorize("hasAuthority('EMPLOYEE')")
-    @PutMapping(path = "/{Iban}", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> updateAccount(@RequestBody Account account, @PathVariable("Iban") String Iban) throws APIException {
+    @PutMapping(path = "/{Iban}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateAccount(@RequestBody Account account, @PathVariable("Iban") String Iban)
+            throws APIException {
         Account _account = accountService.updateAccount(account, Iban);
         return ResponseEntity.status(200).body(_account);
-     
+
     }
 
     // delete /accounts/{Iban}
     @PreAuthorize("hasAuthority('CUSTOMER')")
-    @DeleteMapping(path = "/{Iban}", produces=MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(path = "/{Iban}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteAccount(@PathVariable("Iban") String Iban) throws APIException {
-            accountService.deleteAccount(Iban);
-            return  ResponseEntity.status(204).body(null);
-       
+        accountService.deleteAccount(Iban);
+        return ResponseEntity.status(204).body(null);
+
     }
 
-
-    // /accounts/{iban}/transactions
+    // get /accounts/{iban}/transactions
     @PreAuthorize("hasAuthority('CUSTOMER')")
-    @GetMapping(path = "/{Iban}/transactions", produces=MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/{Iban}/transactions", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getTransactions(@PathVariable("Iban") String Iban) throws APIException {
-            List<Transaction> accounts = accountService.getTransactions(Iban);
-            return ResponseEntity.status(200).body(accounts);
 
-      
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        CustomerIbanCheck(user, Iban);
+        List<Transaction> accounts = accountService.getTransactions(Iban);
+        return ResponseEntity.status(200).body(accounts);
+
     }
 
     // get /accounts/{id}/balance
     @PreAuthorize("hasAuthority('CUSTOMER')")
-    @GetMapping(path = "/{Iban}/balance", produces=MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/{Iban}/balance", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BigDecimal> getBalance(@PathVariable("Iban") String Iban) throws APIException {
-            BigDecimal balance = accountService.getBalance(Iban);
-            return ResponseEntity.status(200).body(balance);
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        CustomerIbanCheck(user, Iban);
+        BigDecimal balance = accountService.getBalance(Iban);
+        return ResponseEntity.status(200).body(balance);
 
-        
+    }
 
+
+    private Account CustomerIbanCheck(User user, String Iban) throws APIException {
+        Account account = accountService.getAccountByIban(Iban)
+                .orElseThrow( () ->new APIException("Account not found", HttpStatus.NOT_FOUND,LocalDateTime.now()));
+        if (account.getUser() != user && user.getRole() == Role.CUSTOMER) {
+            throw new APIException("no acces", HttpStatus.FORBIDDEN, null);
+        }
+        return account;
     }
 }
--- todo.md
+++ todo.md
@@ -1,28 +1,29 @@
 # /accounts
 
+
 ### GET        /accounts -> Employee
 ### GET        /accounts/{iban} -> Customer
 
-- If called by Customer; check if iban in url belongs to userId in JWT.
+- [x] If called by Customer; check if iban in url belongs to userId in JWT.
 
 ### GET        /accounts/{iban}/transactions -> Customer
 
-- If called by Customer; check if iban in url belongs to userId in JWT.
+- [x] If called by Customer; check if iban in url belongs to userId in JWT.
 
 ### GET        /accounts/{iban}/balance -> Customer
 
-- If called by Customer; check if iban in url belongs to userId in JWT.
+- [x] If called by Customer; check if iban in url belongs to userId in JWT.
 
 ### POST        /accounts -> Customer
 
-- Owner of account cannot have isDeleted set to true.
-- If called by Customer; check if userId in request body is equal to userId in JWT.
-- IBAN, balance, and isDeleted cannot be set, but use generated and/or default values.
+- [ ] Owner of account cannot have isDeleted set to true.
+- [ ] If called by Customer; check if userId in request body is equal to userId in JWT.
+- [ ] IBAN, balance, and isDeleted cannot be set, but use generated and/or default values.
 
 ### PUT        /accounts/{iban} -> Employee
 
-- IBAN, balance, user, and isDeleted are not to be changed.
+- [ ] IBAN, balance, user, and isDeleted are not to be changed.
 
 ### DELETE    /accounts/{iban} -> Customer
 
-- If called by Customer; check if iban in url belongs to userId in JWT
+- [ ] If called by Customer; check if iban in url belongs to userId in JWT

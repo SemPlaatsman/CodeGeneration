4df439ce93d0477cdcfda0b235b1ef0e03ea9387
Floortje <florian.tjeertes@ziggo.nl>
2023-06-10 00:07:13 +0200

    added tests

--- src/test/java/nl/inholland/codegeneration/services/UserServiceTest.java
+++ src/test/java/nl/inholland/codegeneration/services/UserServiceTest.java
@@ -9,7 +9,10 @@ import nl.inholland.codegeneration.models.DTO.request.UserRequestDTO;
 import nl.inholland.codegeneration.repositories.AccountRepository;
 import nl.inholland.codegeneration.repositories.TransactionRepository;
 import nl.inholland.codegeneration.repositories.UserRepository;
+import nl.inholland.codegeneration.services.mappers.AccountDTOMapper;
 import nl.inholland.codegeneration.services.mappers.UserDTOMapper;
+
+import org.h2.mvstore.Page;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,16 +22,27 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 
 import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
@@ -44,10 +58,15 @@ public class UserServiceTest {
     UserDTOMapper userDTOMapper;
     @InjectMocks
     UserService userService;
-
+    @Mock
+    private AccountDTOMapper accountDTOMapper;
     @Mock
     PasswordEncoder passwordEncoder;
   
+    User AuthenticationUser = new User(null, null, null, null, null, null, null, null, null, null, null, null);
+
+
+
     @BeforeEach
     public void setup() {
         userDTOMapper = new UserDTOMapper(Mockito.mock(TransactionRepository.class));
@@ -55,7 +74,57 @@ public class UserServiceTest {
         userDTOMapper.toResponseDTO = Mockito.mock(Function.class);
 
         userService = new UserService(userRepository, accountRepository, userDTOMapper, passwordEncoder);
+
+
+
+        AuthenticationUser.setUsername("sarawilson");
+        AuthenticationUser.setPassword("sara123");
+        AuthenticationUser.setRoles(Collections.singletonList(Role.EMPLOYEE)); // Assuming the user has the role
+                                                                               // "EMPLOYEE"
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        AuthenticationUser,
+                        "sara123",
+                        AuthenticationUser.getAuthorities());
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        
+
+
+
     }
+
+ //TODO: Add tests for the getAll method
+    @Test
+    public void testGetAll() throws Exception {
+       // Mock the necessary objects
+       QueryParams<User> queryParams = mock(QueryParams.class);
+       Specification<User> specification = mock(Specification.class);
+       UserRepository userRepository = mock(UserRepository.class);
+       UserDTOMapper userDTOMapper = mock(UserDTOMapper.class);
+       Page<User> userPage = mock(Page.class);
+       List<User> userList = new ArrayList<>();
+       userList.add(new User());
+       when(userPage.getContent()).thenReturn(userList);
+       when(userRepository.findAll(any(Specification.class), any(PageRequest.class))).thenReturn(userPage);
+       when(userDTOMapper.toResponseDTO(any(User.class))).thenReturn(new UserResponseDTO());
+
+       // Create an instance of MyClass
+       V myClass = new MyClass(userRepository, userDTOMapper);
+
+       // Call the method under test
+       List<UserResponseDTO> result = myClass.getAll(queryParams, true);
+
+       // Assert the result
+       assertEquals(1, result.size());
+
+       // Verify the method calls
+       verify(userRepository).findAll(any(Specification.class), any(PageRequest.class));
+       verify(userDTOMapper, times(1)).toResponseDTO(any(User.class));
+    }
+
 
     @Test
     public void testGetById_whenUserExists() {
@@ -114,11 +183,10 @@ public class UserServiceTest {
         User user = new User();
         user.setId(2L);
         when(userDTOMapper.toUser.apply(userRequestDTO)).thenReturn(user);
-        try {
-            userService.update(userRequestDTO, 1L);
-        } catch (InvalidDataAccessApiUsageException e) {
-            // Expected exception
-        }
+        EntityNotFoundException  exception = assertThrows(EntityNotFoundException.class, () ->  userService.update(userRequestDTO, 1L));
+
+        assertEquals("User not found!", exception.getMessage());
+
     }
 
     @Test

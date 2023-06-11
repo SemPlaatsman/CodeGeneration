e661ba9f58209fe291af0c8ff77ce542a28d5b71
Floortje <florian.tjeertes@ziggo.nl>
2023-06-11 18:10:02 +0200

    more tests

--- src/test/java/nl/inholland/codegeneration/configuration/AppConfigTest.java
+++ src/test/java/nl/inholland/codegeneration/configuration/AppConfigTest.java
@@ -1,31 +1,86 @@
 package nl.inholland.codegeneration.configuration;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import nl.inholland.codegeneration.models.User;
+import nl.inholland.codegeneration.repositories.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class AppConfigTest {
-    @Test
-    void testPasswordEncoder() {
-		fail("Not yet implemented");
+  @Mock
+  UserRepository userRepository;
+  
+  @Mock
+  AuthenticationConfiguration authenticationConfiguration;
 
-    }
+  @Mock
+  AuthenticationManager authenticationManager;
 
-    @Test
-    void testAuthenticationManager() {
-		fail("Not yet implemented");
+  @Mock
+  PasswordEncoder passwordEncoder;
 
-    }
+  @Mock
+  UserDetailsService userDetailsService;
 
-    @Test
-    void testAuthenticationProvider() {
-		fail("Not yet implemented");
+  @Mock
+  UserRepository mockUserRepository;
 
-    }
+  @Mock
+  UserDetails userDetails;
 
-    @Test
-    void testUserDetailsService() {
-		fail("Not yet implemented");
+  @Test
+  void testPasswordEncoder() {
 
-    }
+    AppConfig appConfig = new AppConfig(userRepository);
+    PasswordEncoder passwordEncoder = appConfig.PasswordEncoder();
+    assertNotNull(passwordEncoder);
+  }
+
+  @Test
+  void testAuthenticationManager() throws Exception {
+    when(authenticationConfiguration.getAuthenticationManager()).thenReturn(authenticationManager);
+
+    AppConfig appConfig = new AppConfig(mock(UserRepository.class));
+    AuthenticationManager result = appConfig.authenticationManager(authenticationConfiguration);
+
+    assertNotNull(result);
+  }
+
+  @Test
+  void testAuthenticationProvider() {
+
+
+    AppConfig appConfig = new AppConfig(mockUserRepository);
+    AuthenticationProvider authenticationProvider = appConfig.authenticationProvider();
+    assertNotNull(authenticationProvider);
+  }
+
+  @Test
+  void testUserDetailsService() {
+    when(userRepository.findByUsername("test")).thenReturn(Optional.of(new User()));
+
+    AppConfig appConfig = new AppConfig(userRepository);
+    UserDetailsService userDetailsService = appConfig.userDetailsService();
+    UserDetails userDetails = userDetailsService.loadUserByUsername("test");
+
+    assertNotNull(userDetails);
+
+  }
 }

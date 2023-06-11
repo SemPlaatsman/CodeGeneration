c7bea13cbb157aa311a546d6199d533fe4aed591
Floortje <florian.tjeertes@ziggo.nl>
2023-06-11 15:27:23 +0200

    tests

--- src/test/java/nl/inholland/codegeneration/security/JwtFIlterTest.java
+++ src/test/java/nl/inholland/codegeneration/security/JwtFIlterTest.java
@@ -1,5 +1,7 @@
 package nl.inholland.codegeneration.security;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -7,6 +9,11 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,12 +24,15 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nl.inholland.codegeneration.models.Role;
+import nl.inholland.codegeneration.models.User;
 import nl.inholland.codegeneration.services.JwtService;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,6 +56,7 @@ public class JwtFilterTest{
     @Mock
     private Authentication authentication;
 
+
     @InjectMocks
     private JwtFilter jwtFilter;
 
@@ -53,43 +64,47 @@ public class JwtFilterTest{
 
     @BeforeEach
     public void setup() {
+
+        //  jwtService = new JwtService();
     }
 
     
     @Test
     public void testDoFilterInternal_WithValidToken_ShouldSetAuthenticationInSecurityContextHolder() throws ServletException, IOException {
-        String jwt = "validToken";
-        String username = "testUser";
+    String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huZG9lIiwiaWF0IjoxNjg2NDgzNzI2LCJleHAiOjE2ODY1MTk3MjZ9.6oXgxlZBznZ1kUFWN1w5aAKlh0fX0Nu28sxA_xPAw84";
+    String username = "testUser";
 
-        when(request.getHeader("Authorization")).thenReturn("Bearer " + jwt);
-        when(jwtService.extractUsername(jwt)).thenReturn(username);
-        when(userDetailsService.loadUserByUsername(username)).thenReturn(createUserDetails(username));
-        when(jwtService.validateToken(jwt, createUserDetails(username))).thenReturn(true);
-        when(authentication.getPrincipal()).thenReturn(createUserDetails(username));
-        when(authentication.isAuthenticated()).thenReturn(true);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        jwtFilter.doFilterInternal(request, response, filterChain);
-
-        verify(filterChain, times(1)).doFilter(request, response);
-        verify(jwtService, times(1)).extractUsername(jwt);
-        verify(userDetailsService, times(1)).loadUserByUsername(username);
-        verify(jwtService, times(1)).validateToken(jwt, createUserDetails(username));
-        verify(authentication, times(1)).setDetails(any());
-        verify(SecurityContextHolder.getContext(), times(1)).setAuthentication(authentication);
+    User user = new User(1L, List.of(Role.CUSTOMER), username, "sara123", "Sara", "Wilson",
+    "sara.wilson@yahoo.com",
+    "0612345678", LocalDate.of(1990, 11, 13), new BigDecimal(1000), new BigDecimal(200),
+    false);
+   
+    
+    when(request.getHeader("Authorization")).thenReturn("Bearer " + jwt);
+    when(jwtService.extractUsername(jwt)).thenReturn(username);
+    when(userDetailsService.loadUserByUsername(username)).thenReturn(user);
+    when(jwtService.validateToken(jwt, user)).thenReturn(true);
+    
+    jwtFilter.doFilterInternal(request, response, filterChain);
+    
+    verify(filterChain, times(1)).doFilter(request, response);
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    assertNotNull(authentication);
+    assertEquals(username, ((User) authentication.getPrincipal()).getUsername());
     }
+    
 
     @Test
     public void testDoFilterInternal_WithInvalidToken_ShouldContinueFilterChain() throws ServletException, IOException {
         
         String jwt = "invalidToken";
 
-        when(request.getHeader("Authorization")).thenReturn("sBearer " + jwt);
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + jwt);
 
         jwtFilter.doFilterInternal(request, response, filterChain);
 
         verify(filterChain, times(1)).doFilter(request, response);
-        verifyNoMoreInteractions(SecurityContextHolder.getContext());
+
     }
 
     @Test
@@ -99,8 +114,6 @@ public class JwtFilterTest{
         jwtFilter.doFilterInternal(request, response, filterChain);
 
         verify(filterChain, times(1)).doFilter(request, response);
-        // verifyZeroInteractions(jwtService, userDetailsService, authentication);
-        verifyNoMoreInteractions(SecurityContextHolder.getContext());
     }
 
    
